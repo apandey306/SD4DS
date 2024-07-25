174d541c04408c594373581d9dcbc72dfb9af903
Anunaya Pandey <apandey306@gatech.edu>
2024-07-25 14:25:24 +0530

    Pending updates from Lesson 9

--- 11_APIs/chapter_11_api.py
+++ 11_APIs/chapter_11_api.py
@@ -15,6 +15,28 @@ def say_hi():
 # Using type annotations here has a number of benefits. 
 # You can use a type checking tool to avoid common errors and validate your input data. 
 @app.get("/say_hello/{name}")
-def say_hello(name):
+def say_hello(name: str) -> dict:
     return {"Hello": name}
 
+@app.get("/country_trendline/{country}")
+def calculate_country_trendline(country: str):
+    slope, r_squared = country_trendline(country)
+    return {"slope": slope, "r_squared": r_squared}
+
+
+# TODO: Validate the input data to this function using Pydantic.
+class TrendlineInput(BaseModel):
+    timestamps: List[int]
+    data: List[float]
+
+# This function will accept data in JSON format as its input. 
+# You can also improve your API documentation very easily by adding 
+# a summary and description to the @app decorator arguments.
+@app.post(
+    "/fit_trendline/",
+    summary="Fit a trendline to any data",
+    description="Provide a list of integer timestamps and a list of floats",
+)
+def calculate_trendline(trendline_input: TrendlineInput):
+    slope, r_squared = fit_trendline(trendline_input.timestamps, trendline_input.data)
+    return {"slope": slope, "r_squared": r_squared}
